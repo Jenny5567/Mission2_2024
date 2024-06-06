e9617dd345465e3667adab01e06706dc39e2ba87
Jenny5567 <105160294+Jenny5567@users.noreply.github.com>
2024-06-06 18:54:42 +0800

    Delete 2024切版任務 directory

--- 2024切版任務/action.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.btn').click(function(){
-    $('.menu').toggleClass('close');
-    $('.closure').toggleClass('close');
-    $('ul').toggleClass('show');
-  })
-});
